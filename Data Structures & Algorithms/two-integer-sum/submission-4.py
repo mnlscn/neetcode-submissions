500ee5ddccdee749f5e2408c634d860ef8723c0f
Manuel Scionti <93007030+mnlscn@users.noreply.github.com>
2026-07-18 19:13:26 +0200

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic={}
+
+        for i in range(len(nums)):
+            num = nums[i]
+            complement = target - num
+
+            if complement in dic:
+                return [dic[complement], i]
+
+            dic[num] = i
+
+
+
+        
